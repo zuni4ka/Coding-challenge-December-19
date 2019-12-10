8707ec128320ac7d874bb6ef1535bd9c1f477885
Maria Plieshka <zuni4ka@gmail.com>
2019-12-10 22:07:56 +0100

    Define initial state

--- src/App.js
+++ src/App.js
@@ -5,17 +5,101 @@ import {
 } from 'react-router-dom';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route path="/" component={() => 1} exact />
-        <Route path="/step-2" component={() => 2} />
-        <Route path="/step-3" component={() => 3} />
-        <Route path="/step-4" component={() => 4} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      duration: {
+        value: 12,
+        possible_values: [3, 6, 12],
+      },
+      gigabytes: {
+        value: 5,
+        possible_values: [3, 5, 10, 20, 30, 50],
+      },
+      upfrontPayment: {
+        value: false,
+      },
+      firstName: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      lastName: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      email: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      streetAdress: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      cardNumber: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      cardExpDate: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      cardCVV: {
+        value: null,
+        required: true,
+        error: null,
+      },
+      termsAgreement: {
+        value: false,
+      }
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => {
+              return (
+                <div>Step 1</div>
+              );
+            }}
+          />
+          <Route
+            path="/step-2"
+            component={() => {
+              return (
+                <div>Step 2</div>
+              );
+            }} />
+          <Route
+            path="/step-3"
+            component={() => {
+              return (
+                <div>Step 3</div>
+              );
+            }} />
+          <Route
+            path="/step-4"
+            component={() => {
+              return (
+                <div>Step 4</div>
+              );
+            }} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
